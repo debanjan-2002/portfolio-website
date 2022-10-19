ca90a5f8d065ac8c775ae9dab1a9d1b18047d266
Debanjan Poddar <debanjan2602@gmail.com>
2022-10-19 16:35:34 +0530

    Set up flash with express-session

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const path = require("path");
 const engine = require("ejs-mate");
+const session = require("express-session");
+const flash = require("connect-flash");
 const transporter = require("./nodemailer");
 
 const app = express();
@@ -12,10 +14,32 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
+const sessionConfig = {
+    name: "session",
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        // secure: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+};
+
+app.use(session(sessionConfig));
+app.use(flash());
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.engine("ejs", engine);
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
+});
 
 app.get("/", (req, res) => {
     res.render("index");
@@ -26,16 +50,22 @@ app.get("/contact", (req, res) => {
 });
 
 app.post("/contact", async (req, res) => {
-    const { email, message, name } = req.body;
-    const mailDetails = {
-        from: `${name} <${email}>`,
-        to: process.env.AUTH_EMAIL,
-        subject: `Portfolio Website Contact - by ${name}`,
-        text: message,
-        replyTo: `${email}`
-    };
-    await transporter.sendMail(mailDetails);
-    res.redirect("/contact");
+    try {
+        const { email, message, name } = req.body;
+        const mailDetails = {
+            from: `${name} <${email}>`,
+            to: process.env.AUTH_EMAIL,
+            subject: `Portfolio Website Contact - by ${name}`,
+            text: message,
+            replyTo: `${email}`
+        };
+        await transporter.sendMail(mailDetails);
+        req.flash("success", "Email sent successfully!");
+        res.redirect("/contact");
+    } catch (e) {
+        req.flash("error", "Couldn't send email");
+        res.redirect("/contact");
+    }
 });
 
 app.listen(3000);
