aba4b8cfb08a6830cb3ec52dfad945d7c1fd7451
Debanjan Poddar <debanjan2602@gmail.com>
2022-10-20 14:44:26 +0530

    extended tailwind css (animation & keyframes)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@ module.exports = {
     content: ["./views/**/*.ejs", "./public/**/*{.js,.svg}"],
     mode: "jit",
     theme: {
-        extend: {}
+        extend: {
+            animation: {
+                tilt: "tilt 10s infinite linear"
+            },
+            keyframes: {
+                tilt: {
+                    "0% 50% 100%": {
+                        transform: "rotate(0deg)"
+                    },
+                    "25%": {
+                        transform: "rotate(2deg)"
+                    },
+                    "75%": {
+                        transform: "rotate(-2deg)"
+                    }
+                }
+            }
+        }
     },
     plugins: []
 };
