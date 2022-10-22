9ddd33f3fd0892d0097a8c01c8d0d2d4b46c3e66
Debanjan Poddar <debanjan2602@gmail.com>
2022-10-22 23:44:30 +0530

    changed custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,10 @@ module.exports = {
                         transform: "rotate(0deg)"
                     },
                     "25%": {
-                        transform: "rotate(2deg)"
+                        transform: "rotate(1deg)"
                     },
                     "75%": {
-                        transform: "rotate(-2deg)"
+                        transform: "rotate(-1deg)"
                     }
                 }
             }
